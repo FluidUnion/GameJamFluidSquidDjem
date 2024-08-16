6af92ccc587b7d0de020ab941d4946e026df5ab9
squidveemo <k.steevens@sintlucasedu.nl>
2024-08-16 22:57:57 +0200

    first push

---
+++ Assets/Scripts/Kevin/Tilemaster.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilemaster : MonoBehaviour
+{
+    [SerializeField] private int _width, _height;
+
+    [SerializeField] private Tilemaster _TilePrefab;
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++) { 
+        for (int y = 0; y < _height; y++)
+            {
+
+            }
+        
+        }
+    }
+}
