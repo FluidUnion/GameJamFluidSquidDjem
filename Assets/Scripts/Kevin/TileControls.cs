3f04b6c84fb79ede643d33c0745093a8c3e3aceb
Djem <d.thijs@sintlucasedu.nl>
2024-08-17 13:41:44 +0200

    updated kevins code

--- Assets/Scripts/Kevin/TileControls.cs
+++ Assets/Scripts/Kevin/TileControls.cs
@@ -22,6 +22,9 @@ public class TileControls : MonoBehaviour
     //public int speed;
     // Start is called before the first frame update
 
+    public static int height = 20;
+    public static int width = 10;
+
 
     public void OnRight(InputAction.CallbackContext context)
     {
@@ -72,11 +75,22 @@ public class TileControls : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.D))
         {
             transform.position += new Vector3(1, 0, 0);
+
+            if(!ValidMove())
+            {
+                transform.position -= new Vector3(1, 0, 0);
+            }
         }
 
         if (Input.GetKeyDown(KeyCode.A))
         {
             transform.position += new Vector3(-1, 0, 0);
+
+
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(-1, 0, 0);
+            }
         }
     }
     private void FixedUpdate()
@@ -84,6 +98,12 @@ public class TileControls : MonoBehaviour
         if (Time.time - PreviousTime > (Input.GetKey(KeyCode.S) ? FallTime / 10 : FallTime))
         {
             transform.position += new Vector3(0, -1, 0);
+
+            if (!ValidMove())
+            {
+                transform.position -= new Vector3(0, -1, 0);
+            }
+
             PreviousTime = Time.time;
         }
     }
@@ -101,5 +121,19 @@ public class TileControls : MonoBehaviour
             transform.position += new Vector3(-1, 0, 0);
             StartCoroutine(TimeTillMove(PreviousInputTime));
         }
+    }
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int RoundedX = Mathf.RoundToInt(children.transform.position.x);
+            int RoundedY = Mathf.RoundToInt(children.transform.position.y);
+
+            if (RoundedX <0 || RoundedX > width || RoundedY <0 || RoundedY >= height)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
