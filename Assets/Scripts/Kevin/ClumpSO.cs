182444205d45e9c3aea71bbfcbf756f4c583b74e
squidveemo <k.steevens@sintlucasedu.nl>
2024-08-18 21:53:37 +0200

    Hey all, Kevin here

---
+++ Assets/Scripts/Kevin/ClumpSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "SO/ClumpSO")]
+
+public class ClumpSO : ScriptableObject
+{
+    public GameObject SavedShape;
+}
